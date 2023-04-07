b61c197d4faeb14a878e90770723152815cae1f1
Ash <ashplasek@gmail.com>
2023-04-07 15:42:08 -0500

    Add header

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn App() -> Html {
 
     html! {
         <div>
+            <h1>{ "Database Final Project" }</h1>
             <button {onclick}>{ "+1" }</button>
             <p>{ *counter }</p>
         </div>
