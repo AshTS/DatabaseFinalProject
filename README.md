0cd48dfbbafae34ad2504e7b5830aace48c3ad78
Ash <ashplasek@gmail.com>
2023-04-26 11:12:34 -0500

    Interactions with mongodb

--- README.md
+++ README.md
@@ -3,3 +3,23 @@ This is a group final project for 4453 Systems: Databases
 by Isabelle Hjelden, Jadyn Sondrol, and Ash Plasek
 
 (README is undercontruction)
+
+## First Time Build
+
+When building for the first time, the web assembly target must be added via `rustup` to allow the client to build. To do this run
+
+```
+rustup target add wasm32-unknown-unknown
+```
+
+In addition, the client is served using `trunk`, which needs to be installed.
+
+```
+cargo install --locked trunk
+```
+
+## Execution
+
+To build and host the server, run `cargo run` in the `server/` directory.
+
+To build and host the client, run `cargo run` in the `client/` directory. 
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,74 @@
 #[macro_use] 
 extern crate rocket;
-use std::path::{PathBuf};
+use std::{path::{PathBuf}, sync::atomic::Ordering};
 
-use rocket::{serde::json::Json, http::{Header}, Response, fairing::{Fairing, Info, Kind}, Request};
+use mongodb::bson::oid::ObjectId;
+use rocket_db_pools::{Database, Connection};
+use rocket_db_pools::mongodb::*;
+
+use rocket::{serde::json::Json, http::{Header}, Response, fairing::{Fairing, Info, Kind}, Request, State, futures::StreamExt};
 use serde::{Serialize, Deserialize};
+
+use std::sync::atomic::AtomicUsize;
+
+/// Define a type that models our data.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Spell {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<ObjectId>,
+    range: usize, // Feet
+    duration: usize, // In Seconds
+    requirements: ObjectId,
+    level: u8
+}
+
+
+/// Define a type that models our data.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct SpellRequirement {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<ObjectId>,
+    verbal: bool,
+    somatic: bool,
+    material: bool
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+enum Class {
+    Bard,
+    BlackGuard,
+    Cleric,
+    Druid,
+    Paladin,
+    Ranger,
+    Wizard
+}
+
+/// Define a type that models our data.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct SpellClass {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<ObjectId>,
+    class: Class,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+
+#[get("/all")]
+async fn read(mut db: Connection<DataStuff>) -> String {
+    let mut items = db.database("local").collection::<SpellRequirement>("testdb").find(None, None).await.unwrap();
+
+    let mut data = String::new();
+
+    while let Some(doc) = items.next().await {
+        data += &format!("{:?}\n", doc);
+    }
+
+    data
 }
 
 
@@ -33,7 +94,11 @@ impl Fairing for Cors {
     }
 }
 
+#[derive(Database)]
+#[database("database")]
+struct DataStuff(rocket_db_pools::mongodb::Client);
+
 #[launch]
-fn rocket() -> _ {
-    rocket::build().attach(Cors).mount("/", routes![index])
+async fn rocket() -> _ {
+    rocket::build().manage(HitCount { count: AtomicUsize::new(0) }).attach(DataStuff::init()).attach(Cors).mount("/", routes![index, count, read])
 }
